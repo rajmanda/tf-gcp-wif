04bcdc7cbd1429b40e6e6e50f9be23a2bdff6db2
rajmanda <raj.manda@gmail.com>
2024-10-01 20:07:32 -0400

    removing provider block from main as its already given in provider.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.project
-  region  = var.region
-}
-
 resource "google_storage_bucket" "my-tf-gcp-wif-bucket-01" {
   name                        = "tf-gcp-wif-001"
   location                    = "us-east1"
