b5aa15f9e8bdf1019519d305ccbe8853cc01a868
rajmanda <raj.manda@gmail.com>
2024-10-01 20:03:56 -0400

    IAM role assignments included for the dynamically created service account

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
 resource "google_storage_bucket" "my-tf-gcp-wif-bucket-01" {
-  name          = "tf-gcp-wif-001"
-  location      = "us-east1"
-  force_destroy = true
-  public_access_prevention = "enforced"
+  name                        = "tf-gcp-wif-001"
+  location                    = "us-east1"
+  force_destroy               = true
+  public_access_prevention    = "enforced"
 }
 
 resource "google_storage_bucket" "my-tf-gcp-wif-bucket-02" {
-  name          = "tf-gcp-wif-002"
-  location      = "us-east1"
-  force_destroy = true
-  public_access_prevention = "enforced"
+  name                        = "tf-gcp-wif-002"
+  location                    = "us-east1"
+  force_destroy               = true
+  public_access_prevention    = "enforced"
 }
 
 module "kubernetes-engine_example_autopilot_private_firewalls" {
@@ -17,7 +22,19 @@ module "kubernetes-engine_example_autopilot_private_firewalls" {
   version = "33.0.4"
 
   project_id = var.project
-  region  = var.region
+  region     = var.region
 }
 
+# Assign IAM roles to the dynamically created service account
+resource "google_project_iam_member" "cluster_service_account_roles" {
+  for_each = toset([
+    "roles/iam.serviceAccountAdmin",
+    "roles/container.clusterAdmin",
+    "roles/monitoring.metricWriter",
+    "roles/stackdriver.resourceMetadata.writer"
+  ])
 
+  project = var.project
+  role    = each.key
+  member  = "serviceAccount:${module.kubernetes-engine_example_autopilot_private_firewalls.service_account}"
+}
