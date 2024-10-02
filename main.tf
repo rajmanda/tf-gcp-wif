6904c1c4909940e9e3e89a06286c4a694034ea48
rajmanda <raj.manda@gmail.com>
2024-10-01 22:42:06 -0400

    Removing creating bukcets

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-resource "google_storage_bucket" "my-tf-gcp-wif-bucket-01" {
-  name                        = "tf-gcp-wif-001"
-  location                    = "us-east1"
-  force_destroy               = true
-  public_access_prevention    = "enforced"
-}
-
-resource "google_storage_bucket" "my-tf-gcp-wif-bucket-02" {
-  name                        = "tf-gcp-wif-002"
-  location                    = "us-east1"
-  force_destroy               = true
-  public_access_prevention    = "enforced"
-}
-
 module "kubernetes-engine" {
   source  = "terraform-google-modules/kubernetes-engine/google"
   version = "33.0.4"
