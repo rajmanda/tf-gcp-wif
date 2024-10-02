9aeb242c2ba8c67a9439a2814d2a126d4e54c401
rajmanda <raj.manda@gmail.com>
2024-10-01 23:16:29 -0400

    adding depends on

--- main.tf
+++ main.tf
@@ -15,16 +15,15 @@ resource "google_compute_subnetwork" "gke_subnetwork" {
   }
 }
 
-
 module "gke" {
   source  = "terraform-google-modules/kubernetes-engine/google"
   version = "33.0.4"
-  
+
   # Use the variables from vars.tf
   name               = var.name
   project_id         = var.project_id
   network            = var.network
-  subnetwork         = var.subnetwork
+  subnetwork         = google_compute_subnetwork.gke_subnetwork.id  # Reference the created subnetwork
   ip_range_pods      = var.ip_range_pods
   ip_range_services  = var.ip_range_services
 
@@ -33,5 +32,7 @@ module "gke" {
 
   # Optionally, you can pass zones if you have zonal clusters
   zones              = var.zones
-}
 
+  # Add the depends_on attribute
+  depends_on = [google_compute_subnetwork.gke_subnetwork]
+}
