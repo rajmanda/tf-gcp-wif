8a38c0f660d41b6ce8acf499da724b30b49fcaa5
rajmanda <raj.manda@gmail.com>
2025-02-18 11:44:14 -0500

    Update 004-create-certmanager.tf

--- 004-create-certmanager.tf
+++ 004-create-certmanager.tf
@@ -1,13 +1,8 @@
-# Data block to refer to the existing GKE cluster
-data "google_container_cluster" "existing" {
-  name     = "simple-autopilot-public-cluster"  # Replace with your GKE cluster name
-  location = "us-central1"                      # Replace with your cluster location
-}
 
 # cert-manager service account does not have the necessary permissions to create a leader election record in the kube-system namespace - hence creating this namespace.
 resource "kubernetes_namespace" "cert-manager-leader-election" {
   #depends_on = [module.kubernetes-engine_example_simple_autopilot_public]
-  depends_on = [ data.google_container_cluster.existing ]
+  #depends_on = [ data.google_container_cluster.existing ]
   metadata {
     name = "cert-manager-leader-election"
   }
