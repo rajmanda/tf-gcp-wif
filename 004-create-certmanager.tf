8e55e04d5ea2b0feaa7212966fe29c34e1d598bb
rajmanda <raj.manda@gmail.com>
2025-02-18 13:31:43 -0500

    Update 004-create-certmanager.tf

--- 004-create-certmanager.tf
+++ 004-create-certmanager.tf
@@ -1,13 +1,42 @@
 
-# cert-manager service account does not have the necessary permissions to create a leader election record in the kube-system namespace - hence creating this namespace.
-# resource "kubernetes_namespace" "cert-manager-leader-election" {
-#   #depends_on = [module.kubernetes-engine_example_simple_autopilot_public]
-#   #depends_on = [ data.google_container_cluster.existing ]
-#   metadata {
-#     name = "cert-manager-leader-election"
-#   }
-# }
 
+#By using this Terraform configuration, you should be able to resolve the RBAC issue and allow the cert-manager-cainjector service account to create Lease resources for leader election
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"  # Adjust this path if your kubeconfig is located elsewhere
+}
+
+resource "kubernetes_role" "cert_manager_cainjector_leader_election" {
+  metadata {
+    name      = "cert-manager-cainjector-leader-election"
+    namespace = "cert-manager"
+  }
+
+  rule {
+    api_groups = ["coordination.k8s.io"]
+    resources  = ["leases"]
+    verbs      = ["get", "watch", "list", "create", "update", "patch"]
+  }
+}
+
+resource "kubernetes_role_binding" "cert_manager_cainjector_leader_election" {
+  metadata {
+    name      = "cert-manager-cainjector-leader-election"
+    namespace = "cert-manager"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.cert_manager_cainjector_leader_election.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "cert-manager-cainjector"
+    namespace = "cert-manager"
+  }
+}
 
 resource "helm_release" "cert_manager" {
   name       = "cert-manager"
@@ -31,4 +60,9 @@ resource "helm_release" "cert_manager" {
     name  = "webhook.timeoutSeconds"
     value = "4"
   }
+
+  depends_on = [
+    kubernetes_role.cert_manager_cainjector_leader_election,
+    kubernetes_role_binding.cert_manager_cainjector_leader_election
+  ]
 }
