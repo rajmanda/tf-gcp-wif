b53acf969b6864d4c0205a02d325442d9df5b311
rajmanda <raj.manda@gmail.com>
2025-02-18 11:38:56 -0500

    Update 004-create-certmanager.tf

--- 004-create-certmanager.tf
+++ 004-create-certmanager.tf
@@ -1,3 +1,18 @@
+# Data block to refer to the existing GKE cluster
+data "google_container_cluster" "existing" {
+  name     = "simple-autopilot-public-cluster"  # Replace with your GKE cluster name
+  location = "us-central1"                      # Replace with your cluster location
+}
+
+# cert-manager service account does not have the necessary permissions to create a leader election record in the kube-system namespace - hence creating this namespace.
+resource "kubernetes_namespace" "cert-manager-leader-election" {
+  #depends_on = [module.kubernetes-engine_example_simple_autopilot_public]
+  depends_on = [ data.google_container_cluster.existing ]
+  metadata {
+    name = "cert-manager-leader-election"
+  }
+}
+
 resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
@@ -10,5 +25,9 @@ resource "helm_release" "cert_manager" {
   set {
     name  = "installCRDs"
     value = "true"
+  }
+  set {
+    name  = "extraArgs[0]"
+    value = "--leader-election-namespace=cert-manager-leader-election"
   }
 }
