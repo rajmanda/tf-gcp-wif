4ea6b66d65d3bba3af468b6b0427de1004d2ca98
rajmanda <raj.manda@gmail.com>
2025-02-19 12:39:07 -0500

    Update 004-create-certmanager.tf

--- 004-create-certmanager.tf
+++ 004-create-certmanager.tf
@@ -1,3 +1,4 @@
+#helm install   cert-manager jetstack/cert-manager   --namespace cert-manager   --create-namespace   --version v1.11.1   --set installCRDs=true   --set global.leaderElection.namespace=cert-manager
 resource "helm_release" "cert_manager" {
   name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
