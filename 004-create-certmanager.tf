6dbe4dd7fe7a82481b9fd7b9bafda40a989b4ab2
rajmanda <raj.manda@gmail.com>
2025-02-19 00:27:29 -0500

    Update 004-create-certmanager.tf - create issuer

--- 004-create-certmanager.tf
+++ 004-create-certmanager.tf
@@ -1,45 +1,9 @@
-
-
-#By using this Terraform configuration, you should be able to resolve the RBAC issue and allow the cert-manager-cainjector service account to create Lease resources for leader election
-
-#resource "kubernetes_role" "cert_manager_cainjector_leader_election" {
-#  metadata {
-#    name      = "cert-manager-cainjector-leader-election"
-#    namespace = "cert-manager"
-#  }
-#
-#  rule {
-#    api_groups = ["coordination.k8s.io"]
-#    resources  = ["leases"]
-#    verbs      = ["get", "watch", "list", "create", "update", "patch"]
-#  }
-#}
-
-#resource "kubernetes_role_binding" "cert_manager_cainjector_leader_election" {
-#  metadata {
-#   name      = "cert-manager-cainjector-leader-election"
-#    namespace = "cert-manager"
-#  }
-#
-#  role_ref {
-#    api_group = "rbac.authorization.k8s.io"
-#    kind      = "Role"
-#    name      = kubernetes_role.cert_manager_cainjector_leader_election.metadata[0].name
-#  }
-#
-#  subject {
-#    kind      = "ServiceAccount"
-#    name      = "cert-manager-cainjector"
-#   namespace = "cert-manager"
-#  }
-#}
-
 resource "helm_release" "cert_manager" {
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  version    = "v1.17.0"
-  namespace  = "cert-manager"
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  version          = "v1.17.0"
+  namespace        = "cert-manager"
   create_namespace = true
 
   set {
@@ -47,23 +11,39 @@ resource "helm_release" "cert_manager" {
     value = "true"
   }
 
- # set {
- #   name  = "prometheus.enabled"
- #   value = "false"
- # }
-
- # set {
- #   name  = "webhook.timeoutSeconds"
- #   value = "4"
- # }
-
   set {
     name  = "global.leaderElection.namespace"
     value = "cert-manager"
   }
+}
 
-  #depends_on = [
-  #  kubernetes_role.cert_manager_cainjector_leader_election,
-  #  kubernetes_role_binding.cert_manager_cainjector_leader_election
-  #]
+resource "kubernetes_manifest" "letsencrypt_staging_issuer" {
+  depends_on = [helm_release.cert_manager]
+
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Issuer"
+    "metadata" = {
+      "name"      = "letsencrypt-staging"
+      "namespace" = "cert-manager" # make sure the namespace matches where cert-manager is installed
+    }
+    "spec" = {
+      "acme" = {
+        "server" = "https://acme-staging-v02.api.letsencrypt.org/directory"
+        "email"  = "raj.manda@gmail.com" 
+        "privateKeySecretRef" = {
+          "name" = "letsencrypt-staging"
+        }
+        "solvers" = [
+          {
+            "http01" = {
+              "ingress" = {
+                "class" = "nginx" # or the ingress controller you are using
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
 }
