b14f23dfbc81d12e15a861cdc7341d84378a03e5
rajmanda <raj.manda@gmail.com>
2025-07-01 16:59:46 -0400

    Update 003-create-cert-issuer.tf

--- 003-create-cert-issuer.tf
+++ 003-create-cert-issuer.tf
@@ -6,7 +6,7 @@ resource "kubernetes_manifest" "letsencrypt_prod_cluster_issuer" {
     "kind"       = "ClusterIssuer"
     "metadata" = {
       "name" = "letsencrypt-prod"
-      "namespace" = "cert-manager"
+       #"namespace" = "cert-manager" #Resources of type 'cert-manager.io/v1, Kind=ClusterIssuer' cannot have a namespace - hence removing it.
     }
     "spec" = {
       "acme" = {
