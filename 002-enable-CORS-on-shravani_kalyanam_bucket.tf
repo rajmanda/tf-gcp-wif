2e3dd62e05cd95ac1d1dad76d62e8badbc888699
Raj Manda <raj.manda@gmail.com>
2025-10-18 12:11:33 -0400

    creating new bucket

--- 002-enable-CORS-on-shravani_kalyanam_bucket.tf
+++ 002-enable-CORS-on-shravani_kalyanam_bucket.tf
@@ -1,10 +1,15 @@
-resource "google_storage_bucket_cors_configuration" "shravani_cors" {
-  bucket = "shravani_kalyanam_bucket"
+resource "google_storage_bucket" "shravani" {
+  name     = "kalyanam_bucket"
+  location = "US"
 
   cors {
     origin          = ["http://localhost:4200"]
     method          = ["GET", "HEAD", "PUT", "POST", "DELETE", "OPTIONS"]
     response_header = ["Content-Type", "x-goog-resumable", "Range", "Accept", "Authorization", "x-goog-meta-*"]
     max_age_seconds = 3600
+  }
+
+  versioning {
+    enabled = true
   }
 }
