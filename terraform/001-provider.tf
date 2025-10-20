8b97fb4c7abc2bdd69fc93737b32ab0d698ef841
Raj Manda <raj.manda@gmail.com>
2025-10-20 18:06:04 -0400

    configuring backend state

--- terraform/001-provider.tf
+++ terraform/001-provider.tf
@@ -5,9 +5,15 @@ terraform {
       version = ">= 7.7.0"
     }
   }
+
+  backend "gcs" {
+    bucket = "tf-gcp-wif-tfstate"
+    prefix = "terraform/kalyanam/state/" # Static prefix
+  }
 }
 
 provider "google" {
   project = var.project_id
   region  = var.region
 }
+
